4cc1af937f2d882677c97084d84f946b4685c158
sruthikuppa <sruthi1020s@gmail.com>
2015-06-11 22:18:11 -0400

    initial commit

---
+++ MyEx1/MyEx1/scripts/demo.ts
@@ -0,0 +1,22 @@
+﻿
+var stage: createjs.Stage;
+var canvas = document.getElementById("canvas");
+
+
+function init() {
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameloop);
+    main();
+}
+
+function gameloop() {
+    stage.update();
+}
+function main() {
+    console.log("game is running");
+    var helloText: createjs.Text = new createjs.Text("hello sruthi!", "50px consolas", "#FFFF00");
+    helloText.x = 30;
+    helloText.y = 200;
+    stage.addChild(helloText);
+}
